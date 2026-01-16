e013e5dadab1e2b01f0f53ff4dfa4605e304143d
Neel Patil <patilneel288@gmail.com>
2026-01-16 10:46:28 +0530

    changes commit

---
+++ myturbo/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import  {Admin}  from "@repo/ui/admin"
+
+export default function AdminPage(){
+    return(
+      <>
+        <h4>Hi im original admin page</h4> 
+        <Admin/>
+      </>
+    )
+}
---
+++ myturbo/packages/ui/src/admin.tsx
@@ -0,0 +1,10 @@
+
+export function Admin(){
+   return( 
+   <div>
+     <h3>
+        This is the admin page
+     </h3>
+   </div>
+   )
+}
